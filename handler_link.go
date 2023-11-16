50f7216d53c218fe835a328f58bcf3ea31a659ad
redd <redd@redds.be>
2023-11-16 12:28:37 +0100

    Add handler_link.go witch handles the creation of link entries and calling handlerRedirect() specifying a link
    The handlerCreateLink() function will decode a JSON payload sent by the client and extract information from it :
    - A url to point to <- required
    - The length for a random short (max 16, default of 6) <- optional
    - A custom short (max length of 16) <- optional
    - An expiry time in minutes (-1 = never, default of 48 hours)
    It will then check and create defaults for this information and will create a database entry based on them.
    
    The handlerGetLink() function will call handlerRedirect() specifying it a url associated with a short (given by getURL()) that will in turn redirect the client to the url.

---
+++ handler_link.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/dchest/uniuri"
+	"github.com/google/uuid"
+	"github.com/redds-be/rlinks/internal/database"
+	"log"
+	"net/http"
+	"time"
+)
+
+func (apiCfg apiConfig) handlerCreateLink(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Client : %s (%s) accessing '%s' with method '%s'.\n", r.RemoteAddr, r.UserAgent(), r.URL.Path, r.Method)
+	var expireAt time.Time
+
+	// Define the structure of the JSON payload that will be read from the user
+	type parameters struct {
+		Url         string `json:"url"`
+		Length      int    `json:"length"`
+		CustomPath  string `json:"custom_path"`
+		ExpireAfter int    `json:"expire_after"`
+	}
+
+	// Decode the JSON, client error if it can't, most likely an invalid syntax
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, r, 400, "Error parsing JSON : Syntax is probably invalid.")
+		return
+	}
+
+	// Check the expiration time and set it to x minute specified by the user, -1 = never, will default to 48 hours
+	if params.ExpireAfter == -1 {
+		expireAt = time.Date(9999, 12, 30, 23, 59, 59, 59, time.UTC)
+		params.Length = 16
+	} else if params.ExpireAfter <= 0 {
+		expireAt = time.Now().UTC().Add(time.Hour * 24 * 2)
+	} else {
+		expireAt = time.Now().UTC().Add(time.Minute * time.Duration(params.ExpireAfter))
+	}
+
+	// Check the length, will default to 6 if it's inferior or equal to 0 or will default to 16 if it's over 16
+	if params.Length <= 0 {
+		params.Length = 6
+	} else if params.Length > 16 {
+		params.Length = 16
+	}
+
+	// Check the path, will default to a randomly generated one with specified length, if its length is over 16, it will be trimmed
+	if params.CustomPath == "" {
+		// To avoid basically telling the user that the custom path he sent is unavailable despite the fact he hasn't sent a custom path, we check and regenerate it if it's unavailable
+		isShortAvailable := false
+		for !isShortAvailable {
+			log.Println("Generating a new short...")
+			params.CustomPath = uniuri.NewLen(params.Length)
+			log.Println("Checking if the new short is available...")
+			links, err := apiCfg.DB.GetLinks(r.Context())
+			if err != nil {
+				log.Printf("Can't access the links : %s", err)
+				respondWithError(w, r, 500, "The server can't check if the short if available.")
+				return
+			}
+			for _, link := range links {
+				if params.CustomPath == link.Short {
+					log.Println("The generated link is already in use.")
+					isShortAvailable = false
+					continue
+				}
+				isShortAvailable = true
+			}
+		}
+	} else if len(params.CustomPath) > 16 {
+		params.CustomPath = params.CustomPath[:16]
+	}
+
+	// Check if the path is a reserved one, 'status' and 'error' are used to debug, 'garbage' is used to delete expired links
+	if params.CustomPath == "status" || params.CustomPath == "error" || params.CustomPath == "garbage" {
+		respondWithJSON(w, 400, fmt.Sprintf("The path '/%s' is a reserved.", params.CustomPath))
+		return
+	}
+
+	// Insert the information to the database, error if it can't, most likely that the short is already in use
+	link, err := apiCfg.DB.CreateLink(r.Context(), database.CreateLinkParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		ExpireAt:  expireAt,
+		Url:       params.Url,
+		Short:     params.CustomPath,
+	})
+	if err != nil {
+		respondWithError(w, r, 400, "Could not add link: The path is probably already in use.")
+		return
+	}
+
+	// Send back the expiry time, the url and the short to the user
+	respondWithJSON(w, 201, databaseLinkToLink(link))
+}
+
+func (apiCfg apiConfig) handlerGetLink(w http.ResponseWriter, r *http.Request, link database.Link) {
+	log.Printf("Client : %s (%s) accessing '%s' with method '%s'.\n", r.RemoteAddr, r.UserAgent(), r.URL.Path, r.Method)
+	// Redirect the client to the URL associated with the short of the database
+	handlerRedirect(w, r, databaseLinkToLink(link).Url)
+}
