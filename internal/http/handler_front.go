aeb0b79972e969e8a3a4d42f1253a3fb5769655e
Ada <ada@gnous.eu>
2024-07-01 14:27:13 +0200

    Remove unnecessary directives from CSP and set default-src to none

--- internal/http/handler_front.go
+++ internal/http/handler_front.go
@@ -79,9 +79,8 @@ func RenderTemplate(writer http.ResponseWriter, tmpl string, page *Page, code in
 	writer.Header().Set("Content-Type", "text/html; charset=UTF-8")
 	// Tell that all resources comes from here and that only this site can frame itself
 	writer.Header().
-		Set("Content-Security-Policy", "default-src 'self'; script-src 'self';"+
-			" style-src 'self'; img-src 'self'; connect-src 'self'; frame-src 'self'; font-src 'self'; media-src 'self';"+
-			" object-src 'self'; manifest-src 'self'; worker-src 'self'; form-action 'self'; frame-ancestors 'self'")
+		Set("Content-Security-Policy", "default-src 'none'; script-src 'self';"+
+			"style-src 'self'; img-src 'self';")
 	// Block access to styles and scripts
 	writer.Header().Set("X-Content-Type-Options", "nosniff")
 
